e607ffdfe415690a6f1b915d54db94282b2061d5
Mirza Arcos <166764651+Arcos-Mir@users.noreply.github.com>
2024-05-25 23:15:15 -0600

    Add files via upload

---
+++ while.js
@@ -0,0 +1,40 @@
+//estructura de while
+/*while (condicion) {
+    //codigo
+} NOTA: la variable se define fuera del while antes de empezar con el ciclo*/
+
+// ----- Ciclo 1 al 10
+
+/*var num = 1;//inicio
+while(num <= 10) {//condicion
+    document.write(num + '<br>');
+    //num = num + 1 *actualizacion
+    num++;//actualizacion
+}*/
+
+// ----- Ciclo infinito, no hacer ya que se puede detener la computadora
+
+/*var verdadero = true;
+while(num <= 10) {
+    while (verdadero) {
+        //codigo
+    }
+}*/
+
+// ----- Ciclo do while, nos permite ejecutar el codigo al menos una vez
+
+/*var numero = 1;
+do {
+    //codigo se repite al menos una vez
+    console.log (numero);
+    numero = numero + 1;
+} while (numero <= 10);*/
+
+var numero = 11;
+do {
+    //codigo se repite al menos una vez
+    console.log (numero);
+    numero = numero + 1;
+} while (numero <= 10);
+
+
